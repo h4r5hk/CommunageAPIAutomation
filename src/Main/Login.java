c758e18d6ff1bab43d3ed8b7196282d8d4c39e72
Harsh shah <h4r5hk@gmail.com>
2022-12-09 18:44:10 +0530

    Changes

--- src/Main/Login.java
+++ src/Main/Login.java
@@ -5,7 +5,7 @@ public class Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is my first program...	");
+		System.out.println("First program...	");
 
 	}
 
